7010916a242c96f2e2c058f18a4e94ea51d197a4
Marty Pitt <martypitt@gmail.com>
2024-09-09 18:43:55 +0100

    docs: added first-pass docs

---
+++ docs/theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+    logo: <span>Nebula</span>,
+    project: {
+      link: 'https://github.com/orbitalapi/nebula'
+    },
+    docsRepositoryBase: 'https://github.com/orbitalapi/nebula/docs'
+    // ... other theme options
+  }
