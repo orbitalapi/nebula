2b0f82addd37bf9d2ddeb22667a3a0bcbd240ef3
Marty Pitt <martypitt@gmail.com>
2024-12-13 15:05:41 +1100

    replace nextra footer with orbital, which required upgrading nextra to 3

--- docs/pages/_meta.ts
+++ docs/pages/_meta.ts
@@ -1,4 +1,4 @@
-{
+export default {
   "index" : "Getting started",
   "stacks" : "Working with stacks"
-}
+};
---
+++ docs/theme.config.tsx
@@ -0,0 +1,14 @@
+export default {
+    logo: <span>Nebula</span>,
+    project: {
+        link: 'https://github.com/orbitalapi/nebula'
+    },
+    docsRepositoryBase: 'https://github.com/orbitalapi/nebula/docs',
+    footer: {
+        content: (
+            <span>Built by &nbsp;<a className="underline" href="https://orbitalhq.com" target="_blank">Orbital</a>, to make demos suck less.
+      </span>
+        )
+    }
+    // ... other theme options
+}
